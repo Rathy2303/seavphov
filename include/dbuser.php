8a6e165ae98a12ea91c1d5e04dfef067952f1f4e
rathy2303 <thyjks2020@gmail.com>
2024-01-23 19:03:26 +0700

    Change dbuser.php connection

--- include/dbuser.php
+++ include/dbuser.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
-    $db=mysqli_connect('sql308.infinityfree.com','if0_35029592','thy23032003','if0_35029592_ebook') or die("Database is not connect !");
+    $db=mysqli_connect('localhost','root','','ebook') or die("Database is not connect !");
 ?>
