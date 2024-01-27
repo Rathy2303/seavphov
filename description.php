42f5d705cdedfe5dd5158caf4b55bd7dfdafc2a1
rathy2303 <thyjks2020@gmail.com>
2024-01-27 22:27:22 +0700

    Finish Client Page

--- description.php
+++ description.php
@@ -35,83 +35,77 @@ include 'include/db.php';
 	<link href="css/mainstyle.css" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 	<link rel="stylesheet" href="css/btn.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 	<!-- -----------------------------------jquery menu-------------------------------------- -->
 </head>
 
-<body class="video-watch videoid-4772 author-1 source-0 featured">
-	<!-- Facebook Share button -->
-	<div id="fb-root"></div>
-	<script async defer crossorigin="anonymous" src="../connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0"></script>
-	<!-- Facebook Share button -->
-	<div id="main_content">
-		<div id="main_content1">
-			<div class="wrap">
-				<?php
-				include 'include/header.php';
-				?>
-				<div class="menu">
-						<div id='cssmenu'>
-							<ul>
-								<li><a href='./index.php'class="active"><span>Home</span></a></li>
-								<?php
-								$menu = "SELECT * FROM menu";
-								$runPQ = mysqli_query($db, $menu);
-								while ($post = mysqli_fetch_assoc($runPQ)) {
-								?>
-									<li><a title="<?= $post['title'] ?>" href='./type.php?category=<?= $post['url'] ?>'><span><?= $post['name'] ?></span></a></li>
-								<?php
-								}
-								?>
-
-							</ul>
-						</div>
-
+<body>
+	<div class="container">
+		<div class="wrap">
+			<?php
+			include 'include/header.php';
+			?>
+			<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+				<div class="container-fluid">
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+							<li class="nav-item">
+								<a class="nav-link active" aria-current="page" href="#">Home</a>
+							</li>
+							<?php
+							$menu = "SELECT * FROM menu";
+							$runPQ = mysqli_query($db, $menu);
+							while ($post = mysqli_fetch_assoc($runPQ)) {
+							?>
+								<li class="nav-item">
+									<a title="<?= $post['title'] ?>" class="nav-link" href='./type.php?category=<?= $post['url'] ?>'><?= $post['name'] ?></a>
+								</li>
+							<?php
+							}
+							?>
+						</ul>
+						<form class="d-flex" role="search">
+							<input class="form-control me-2" type="text" class="search" id="searchid" name="search" placeholder="Search here..." aria-label="Search">
+							<button class="btn btn-light" type="submit">Search</button>
+						</form>
 					</div>
-
-			</div>
-			<!-- -------------------------ad-------------------------------------------------- -->
-			<div class="wrap">
-				<!--	<div class="ad_left"></div>
-			<div class="ad_right"></div>-->
-			</div>
-			<!-- -------------------------end ad-------------------------------------------------- -->
-			<!-- -------------------------content-------------------------------------------------- -->
-			<div class="wrap">
-				<!-- ---------------------------left-------------------------------------------------- -->
-				<div class="left">
-
-					<!-- -------------------------------end new content--------------------------------------- -->
-
-
-					<div class="movie_des">
-						<?php
-						$post_id = $_GET['id'];
-						mysqli_query($db, "UPDATE book SET view=view+1 WHERE id=$post_id");
-						$postQuery = "SELECT * FROM book WHERE id=$post_id";
-						$runPQ = mysqli_query($db, $postQuery);
-						$post = mysqli_fetch_assoc($runPQ);
-						?>
-						<div style="width:100%; background:#0075B0; margin-top:-12px;">
-							<h1 style="margin-left:10px; font-size:18px;padding:10px 0;color:white;">
-								<?= $post['title'] ?>
-							</h1>
+				</div>
+			</nav>
+		</div>
+		<!-- -------------------------content-------------------------------------------------- -->
+		<div class="row" style="margin-top: 20px;">
+			<!-- ---------------------------left-------------------------------------------------- -->
+			<div class="col-xxl-8 col-xl-8 col-lg-8 col-md-12">
+				<!-- -------------------------------end new content--------------------------------------- -->
+				<div class="movie_des">
+					<?php
+					$post_id = $_GET['id'];
+					mysqli_query($db, "UPDATE book SET view=view+1 WHERE id=$post_id");
+					$postQuery = "SELECT * FROM book WHERE id=$post_id";
+					$runPQ = mysqli_query($db, $postQuery);
+					$post = mysqli_fetch_assoc($runPQ);
+					?>
+					<div style="width:100%; background:#0075B0; margin-top:-12px;">
+						<h1 style="margin-left:10px; font-size:18px;padding:10px 0;color:white;">
+							<?= $post['title'] ?>
+						</h1>
+					</div>
+					<div class="row detail-wrapper">
+						<div class="col-6">
+							<img src="images/book/<?= $post['image'] ?>" alt="<?= $post['title'] ?>" class="img-detail" />
 						</div>
-
-						<img src="images/book/<?= $post['image'] ?>" alt="<?= $post['title'] ?>" style="margin-left:10px;border-radius: 10px;width:300px;height:263px;object-fit: cover" />
-
-						<div class="des">
+						<div class="col-6 des">
 							<input type="hidden" id="downloadid" name="downloadid" value="105" />
-
-
-							<p style="padding:0; margin:0;float:left; font-size:15px"><strong>Title : </strong></p>
+							<p style="padding:0; margin:0;; font-size:15px"><strong>Title : </strong></p>
 							<h4 style="padding:0; margin:0; font-size:15px">&nbsp; <?= $post['title'] ?></h4>
-
 							<p style="padding:0; margin-top: 5px; font-size:14px">
 								<strong>Views : </strong> <b style="color: #b84c4c;">
 									<?= $post['view'] ?>&ensp;Times
 								</b>
 							</p>
-
 							<p style="padding:0; margin:0;font-size:14px">
 								<strong>Downloaded : </strong>
 								<?= $post['download'] ?>
@@ -121,120 +115,63 @@ include 'include/db.php';
 								<!-- <img style="float:left" src="images/download.jpg" width="150" /> -->
 								<button class="button-50" role="button">Download</button>
 							</a>
-								
-							<div style="margin-top: -10px;">
-							</div>
 						</div>
-
-						<?php
-
-						?>
-
-
-
 					</div>
-
-
-					<div class="content_title">
-						<h4>Related Books</h4>
-					</div>
-					<div class="New_content_1">
-
-						<!-- ------movie -------- -->
-
-						<?php
-						$pquery = "SELECT*FROM book WHERE category_id={$post['category_id']} ORDER BY id DESC LIMIT 7";
-						$prun = mysqli_query($db, $pquery);
-						while ($rpost = mysqli_fetch_assoc($prun)) {
-							if ($rpost['id'] == $post_id) {
-								continue;
-							}
-						?>
-							<div class="movie_1 center">
-								<a href="description.php?id=<?= $rpost['id'] ?>">
-									<div class="movie_image">
-										<img alt="<?= $rpost['title'] ?>" src="images/book/<?= $rpost['image'] ?>" />
-									</div>
-									<div class="movie_title_1">
-										<span>
-											<?= $rpost['title'] ?>
-										</span><br />
-										<span style="color:#3c763d">
-											<?= $rpost['view'] ?>&nbsp;<i class="fa-solid fa-eye"></i>
-										</span>
-									</div>
-								</a>
-							</div>
-							<!-- ------end movie -------- -->
-
-						<?php
-						}
-						?>
-
-					</div>
-
-
-					<!-- -------------------------------new content_1--------------------------------------- -->
-
-				</div>
-				<!-- -----------------------------end left-------------------------------------------- -->
-				<div class="right">
-					<!-- ------------------search ------------------------ -->
-
-
-					<style type="text/css">
-						.content {
-							width: 900px;
-							margin: 0 auto;
-						}
-
-						#searchid {
-							font-size: 14px;
-						}
-
-						#result {
-							background-color: white;
-							border: 1px solid #ccc;
-							border-radius: 5px;
-							display: none;
-							margin-top: 18px;
-							overflow: hidden;
-							padding: 10px;
-							position: absolute;
-							width: 277px;
-						}
-
-						.show {
-							padding: 10px;
-							border-bottom: 1px #999 dashed;
-							font-size: 15px;
-							height: 50px;
-						}
-
-						.show:hover {
-							background: #4c66a4;
-							color: #FFF;
-							cursor: pointer;
-						}
-					</style>
-
 					<?php
-					include 'include/searchbar.php';
-					include 'include/right_content.php';
+
 					?>
 				</div>
+				<div class="content_title">
+					<h4>Related Books</h4>
+				</div>
+				<div class="row">
+					<!-- ------movie -------- -->
+					<?php
+					$pquery = "SELECT*FROM book WHERE category_id={$post['category_id']} ORDER BY id DESC LIMIT 7";
+					$prun = mysqli_query($db, $pquery);
+					while ($rpost = mysqli_fetch_assoc($prun)) {
+						if ($rpost['id'] == $post_id) {
+							continue;
+						}
+					?>
+						<div class="col-xxl-3 col-xl-3 col-lg-4 col-md-4 col-sm-6 col-6">
+							<a style="text-decoration: none;" href="description.php?id=<?= $rpost['id'] ?>">
+								<div class="movie_image">
+									<img alt="<?= $rpost['title'] ?>" src="images/book/<?= $rpost['image'] ?>" />
+								</div>
+								<div class="movie_title_1">
+									<span>
+										<?= $rpost['title'] ?>
+									</span><br />
+									<span style="color:#3c763d">
+										<?= $rpost['view'] ?>&nbsp;<i class="fa-solid fa-eye"></i>
+									</span>
+								</div>
+							</a>
+						</div>
+						<!-- ------end movie -------- -->
+					<?php
+					}
+					?>
+				</div>
+				<!-- -------------------------------new content_1--------------------------------------- -->
 			</div>
-
+			<!-- -----------------------------end left-------------------------------------------- -->
+			<div class="col-xxl-4 col-xl-4 col-lg-4 col-md-12 most-view">
+				<!-- ------------------search ------------------------ -->
+				<?php
+				include 'include/right_content.php';
+				?>
+			</div>
 		</div>
-		<!-- -------------------------end content-------------------------------------------------- -->
-
-		<?php
-		include 'include/footer.php';
-		include 'include/gotop.php';
-		?>
+	</div>
+	<!-- -------------------------end content-------------------------------------------------- -->
+	<?php
+	include 'include/footer.php';
+	include 'include/gotop.php';
+	?>
 	</div>
 	</div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
-
-
 </html>
--- type.php
+++ type.php
@@ -22,188 +22,147 @@ $result = ($page - 1) * $post_per_page;
     <link rel="shortcut icon" href="kh.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="Switch multiple languages in database,Upload Multiple Images in Advance Laravel,Advance Laravel CRUD Example,How to switch languages in Advance Laravel,Advance Laravel Autocomplete Search from Database,Advance Laravel JQuery Form Validation,How to Create Captcha in Advance Laravel,Ajax Pagination Advance Laravel,Laravel - Dynamically Add or Remove input fields using JQuery,Laravel Ecommerce Full Source Code Project,Basic Laravel," />
-
     <title><?= strtoupper($category) ?></title>
     <link href="css/mainstyle.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <!-- -----------------------------------jquery menu-------------------------------------- -->
     <script src="js/jquery-1.8.0.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- -----------------------------------end jquery menu-------------------------------------- -->
 </head>
 
 <body>
-    <div id="main_content">
-        <div id="main_content1">
-            <div class="wrap">
-                <?php
-                include 'include/header.php';
-                ?>
-                <div class="menu">
-                    <div id='cssmenu'>
-                        <ul>
-                            <li><a href='./index.php' class=""><span>Home</span></a></li>
+    <div class="container">
+        <div class="wrap">
+            <?php
+            include 'include/header.php';
+            ?>
+            <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="./index.php">Home</a>
+                            </li>
                             <?php
                             $menu = "SELECT * FROM menu";
                             $runPQ = mysqli_query($db, $menu);
                             while ($post = mysqli_fetch_assoc($runPQ)) {
                                 if ($post['url'] == $category) {
                             ?>
-                                    <li><a title="<?= $post['title'] ?>" class="active" href='./type.php?category=<?= $post['url'] ?>'><span><?= $post['name'] ?></span></a></li>
-
+                                    <li class="nav-item"><a class="nav-link active" title="<?= $post['title'] ?>" href='./type.php?category=<?= $post['url'] ?>'><span><?= $post['name'] ?></span></a></li>
                                 <?php
                                 } else {
                                 ?>
-                                    <li><a title="<?= $post['title'] ?>" href='./type.php?category=<?= $post['url'] ?>'><span><?= $post['name'] ?></span></a></li>
+                                    <li class="nav-item"><a class="nav-link" title="<?= $post['title'] ?>" href='./type.php?category=<?= $post['url'] ?>'><span><?= $post['name'] ?></span></a></li>
                             <?php
                                 }
                             }
                             ?>
                         </ul>
-                    </div>
-
-                </div>
-            </div>
-            <!-- -------------------------ad-------------------------------------------------- -->
-            <div class="wrap">
-            </div>
-            <div class="wrap">
-                <!-- ---------------------------left-------------------------------------------------- -->
-                <div class="left">
-                    <div class="content_title">
-                        <h4><?= strtoupper($category) ?></h4>
-                    </div>
-                    <div class="New_content_1">
-                        <?php
-                        if (isset($_GET['search'])) {
-                            $search =  $_POST['search'];
-                            $book = mysqli_query($db, "SELECT book.id,book.view,book.title,book.image FROM book INNER JOIN category ON book.category_id = category.id WHERE category.name='$category' AND book.title LIKE '%$search%' ORDER BY id DESC LIMIT $result,$post_per_page");
-                        } else
-                            $book = mysqli_query($db, "SELECT book.id,book.view,book.title,book.image FROM book INNER JOIN category ON book.category_id = category.id WHERE category.name='$category' ORDER BY id DESC LIMIT $result,$post_per_page");
-
-                        while ($row = mysqli_fetch_assoc($book)) {
-                        ?>
-                            <!-- ------movie -------- -->
-                            <div class="movie_1 center">
-                                <div class="movie_image">
-                                    <img alt="<?= $row['title'] ?>" src="images/book/<?= $row['image'] ?>" />
-                                    <div class="view-detial">
-                                        <a title="<?= $row['title'] ?>" href="description.php?id=<?= $row['id'] ?>"><button>View</button> </a>
-
-                                    </div>
-                                </div>
-                                <div class="movie_title_1">
-                                    <span><?= $row['title'] ?></span><br />
-                                    <span style="color:#3c763d"><?= $row['view'] ?>&nbsp;<i class="fa-solid fa-eye"></i></span>
-                                </div>
-
-                            </div>
-                            <!-- ------end movie -------- -->
-                        <?php
-                        }
-                        // -- ORDER BY id DESC LIMIT $result,$post_per_page"
-                        // $prun = mysqli_query($db, $pquery);
-                        $getpage = mysqli_query($db, "SELECT*FROM book WHERE category_id=1");
-                        $total_post = mysqli_num_rows($getpage);
-                        $total_pages = ceil($total_post / $post_per_page);
-                        ?>
-                    </div>
-
-                    <!-- pagination -->
-                    <div class="pagination_center">
-                        <div class="pagination">
-                            <?php
-                            if ($page > 1) {
-                                $switch = "";
-                            } else {
-                                $switch = "diable_page";
-                            }
-                            if ($page < $total_pages) {
-                                $nswitch = "";
-                            } else {
-                                $nswitch = "diable_page";
-                            }
-                            ?>
-                            <a href="type.php?category=<?= $category ?>&page=<?= $page - 1 ?>" class="<?= $switch ?>">&laquo;</a>
-                            <?php
-                            $c = "active page";
-                            $count = 1;
-                            for ($opage = 1; $opage <= $total_pages; $opage++) {
-                                if ($page == $count) {
-                                    $c = "active page";
-                                } else
-                                    $c = "";
-                            ?>
-                                <a href="?<?php if (isset($_GET['search'])) {
-                                                echo "type.php?category=$category&search=$keyword&";
-                                            } ?>category=<?= $category ?>&page=<?= $count ?>" class="<?= $c ?>"><?= $count ?></a>
-                                <!-- <a href="?page=<?= $opage + 1 ?>" class="<?= $c ?>"><?= $opage + 1 ?></a> -->
-                            <?php
-                                $count++;
-                            }
-                            ?>
-                            <a href="type.php?category=<?= $category ?>&page=<?= $page + 1 ?>" class="<?= $nswitch ?>">&raquo;</a>
-                        </div>
-
-                    </div>
-
-                    <!-- pagination -->
-
-                    <!-- -------------------------------new content_1--------------------------------------- -->
-
-                </div>
-                <!-- -----------------------------end left-------------------------------------------- -->
-                <div class="right">
-                    <style type="text/css">
-                        .content {
-                            width: 900px;
-                            margin: 0 auto;
-                        }
-                        #searchid {
-                            font-size: 14px;
-                        }
-                        #result {
-                            background-color: white;
-                            border: 1px solid #ccc;
-                            border-radius: 5px;
-                            display: none;
-                            margin-top: 18px;
-                            overflow: hidden;
-                            padding: 10px;
-                            position: absolute;
-                            width: 277px;
-                        }
-                        .show {
-                            padding: 10px;
-                            border-bottom: 1px #999 dashed;
-                            font-size: 15px;
-                            height: 50px;
-                        }
-
-                        .show:hover {
-                            background: #4c66a4;
-                            color: #FFF;
-                            cursor: pointer;
-                        }
-                    </style>
-                    <!-- Search Bar -->
-                    <div class="right_content">
-                        <form class="form-wrapper cf" action="?category=<?= $category ?>&search" method="post">
-                            <input type="text" class="search" id="searchid" name="search" placeholder="Search here..." />
-                            <button type="submit">Search</button>
-                            <br />
-                            <div id="result">
+                        <form class="d-flex" role="search" action="?category=<?= $category ?>&search" method="post">
+                            <input class="form-control me-2" type="text" class="search" id="searchid" name="search" placeholder="Search here...">
+                            <button class="btn btn-light" type="submit">Search</button>
                         </form>
                     </div>
+                </div>
+            </nav>
+        </div>
+        <!-- -------------------------ad-------------------------------------------------- -->
+        <div class="row" style="margin-top: 20px;">
+            <!-- ---------------------------left-------------------------------------------------- -->
+            <div class="col-xxl-8 col-xl-8 col-lg-12 col-md-12">
+                <div class="content_title">
+                    <h4><?= strtoupper($category) ?></h4>
+                </div>
+                <div class="row">
                     <?php
-                    include 'include/right_content.php';
+                    if (isset($_GET['search'])) {
+                        $search =  $_POST['search'];
+                        $book = mysqli_query($db, "SELECT book.id,book.view,book.title,book.image FROM book INNER JOIN category ON book.category_id = category.id WHERE category.name='$category' AND book.title LIKE '%$search%' ORDER BY id DESC LIMIT $result,$post_per_page");
+                    } else
+                        $book = mysqli_query($db, "SELECT book.id,book.view,book.title,book.image FROM book INNER JOIN category ON book.category_id = category.id WHERE category.name='$category' ORDER BY id DESC LIMIT $result,$post_per_page");
+
+                    while ($row = mysqli_fetch_assoc($book)) {
+                    ?>
+                        <!-- ------movie -------- -->
+                        <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-6 col-6">
+                            <div class="movie_image">
+                                <img alt="<?= $row['title'] ?>" src="images/book/<?= $row['image'] ?>" />
+                                <div class="view-detial">
+                                    <a title="<?= $row['title'] ?>" href="description.php?id=<?= $row['id'] ?>"><button>View</button> </a>
+
+                                </div>
+                            </div>
+                            <div class="movie_title_1">
+                                <span><?= $row['title'] ?></span><br />
+                                <span style="color:#3c763d"><?= $row['view'] ?>&nbsp;<i class="fa-solid fa-eye"></i></span>
+                            </div>
+
+                        </div>
+                        <!-- ------end movie -------- -->
+                    <?php
+                    }
+                    // -- ORDER BY id DESC LIMIT $result,$post_per_page"
+                    // $prun = mysqli_query($db, $pquery);
+                    $getpage = mysqli_query($db, "SELECT*FROM book WHERE category_id=1");
+                    $total_post = mysqli_num_rows($getpage);
+                    $total_pages = ceil($total_post / $post_per_page);
                     ?>
                 </div>
+                <!-- -------------------------------new content_1--------------------------------------- -->
             </div>
-            <?php
-            include 'include/footer.php';
-            include 'include/gotop.php';
-            ?>
+            <!-- -----------------------------end left-------------------------------------------- -->
+            <div class="col-xxl-4 col-xl-4 col-lg-12 most-view">
+                <?php
+                include 'include/right_content.php';
+                ?>
+            </div>
         </div>
+
+        <div class="pagination_center">
+            <div class="pagination">
+                <?php
+                if ($page > 1) {
+                    $switch = "";
+                } else {
+                    $switch = "diable_page";
+                }
+                if ($page < $total_pages) {
+                    $nswitch = "";
+                } else {
+                    $nswitch = "diable_page";
+                }
+                ?>
+                <a href="type.php?category=<?= $category ?>&page=<?= $page - 1 ?>" class="<?= $switch ?>">&laquo;</a>
+                <?php
+                $c = "active page";
+                $count = 1;
+                for ($opage = 1; $opage <= $total_pages; $opage++) {
+                    if ($page == $count) {
+                        $c = "active page";
+                    } else
+                        $c = "";
+                ?>
+                    <a href="?<?php if (isset($_GET['search'])) {
+                                    echo "type.php?category=$category&search=$keyword&";
+                                } ?>category=<?= $category ?>&page=<?= $count ?>" class="<?= $c ?>"><?= $count ?></a>
+                    <!-- <a href="?page=<?= $opage + 1 ?>" class="<?= $c ?>"><?= $opage + 1 ?></a> -->
+                <?php
+                    $count++;
+                }
+                ?>
+                <a href="type.php?category=<?= $category ?>&page=<?= $page + 1 ?>" class="<?= $nswitch ?>">&raquo;</a>
+            </div>
+        </div>
+        <?php
+        include 'include/footer.php';
+        include 'include/gotop.php';
+        ?>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
